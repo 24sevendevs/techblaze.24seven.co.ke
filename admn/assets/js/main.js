4aeadef5c89f3732ec070472f3051da179f346a7
njoro <giennjoro@24seven.co.ke>
2019-07-07 23:11:01 +0300

    .

--- admn/assets/js/main.js
+++ admn/assets/js/main.js
@@ -205,7 +205,7 @@
         boundary: "window"
     });
 
-    /*-- 
+    /*--
     Selectable Table
 -----------------------------------*/
     function tableSelectable() {
@@ -269,7 +269,7 @@
     }
     tableSelectable();
 
-    /*-- 
+    /*--
     To Do List
 -----------------------------------*/
     function todoList() {
@@ -398,188 +398,6 @@
         resize();
     });
 
-    function customizer() {
-        var $cusHtml = '<div class="customizer-wrapper">';
-        $cusHtml +=
-            '<a href="#" class="customizer-toggle"><i class="ti-settings"></i></a>';
-        $cusHtml += '<div class="inner custom-scroll">';
-        $cusHtml += '<div class="dark-rtl-setting">';
-        $cusHtml +=
-            '<label class="adomx-switch primary toggle-dark"><input type="checkbox"> <i class="lever"></i> <span class="text">Dark Version</span></label>';
-        $cusHtml +=
-            '<label class="adomx-switch primary toggle-rtl"><input type="checkbox" checked> <i class="lever"></i> <span class="text">RTL Version</span></label>';
-        $cusHtml += "</div>";
-        $cusHtml += '<ul class="nav mb-20">';
-        $cusHtml +=
-            '<li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#sidebar-setting">Sidebar</a></li>';
-        $cusHtml +=
-            '<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#header-setting">Header</a></li>';
-        $cusHtml +=
-            '<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#primary-setting">Primary</a></li>';
-        $cusHtml += "</ul>";
-        $cusHtml += '<div class="tab-content">';
-        $cusHtml +=
-            '<div class="tab-pane fade show active" id="sidebar-setting">';
-        $cusHtml += '<h5 class="mb-20">Sidebar Skin Color</h5>';
-        $cusHtml +=
-            '<button class="cus-radio light" data-target="side-header" data-color="light"><i class="icon"></i>light</button>';
-        $cusHtml +=
-            '<button class="cus-radio dark active" data-target="side-header" data-color="dark"><i class="icon"></i>dark</button>';
-        $cusHtml +=
-            '<button class="cus-radio primary" data-target="side-header" data-color="primary"><i class="icon"></i>primary</button>';
-        $cusHtml +=
-            '<button class="cus-radio secondary" data-target="side-header" data-color="secondary"><i class="icon"></i>secondary</button>';
-        $cusHtml +=
-            '<button class="cus-radio indigo" data-target="side-header" data-color="indigo"><i class="icon"></i>indigo</button>';
-        $cusHtml +=
-            '<button class="cus-radio purple" data-target="side-header" data-color="purple"><i class="icon"></i>purple</button>';
-        $cusHtml +=
-            '<button class="cus-radio pink" data-target="side-header" data-color="pink"><i class="icon"></i>pink</button>';
-        $cusHtml +=
-            '<button class="cus-radio red" data-target="side-header" data-color="red"><i class="icon"></i>red</button>';
-        $cusHtml +=
-            '<button class="cus-radio brown" data-target="side-header" data-color="brown"><i class="icon"></i>brown</button>';
-        $cusHtml +=
-            '<button class="cus-radio orange" data-target="side-header" data-color="orange"><i class="icon"></i>orange</button>';
-        $cusHtml +=
-            '<button class="cus-radio green" data-target="side-header" data-color="green"><i class="icon"></i>green</button>';
-        $cusHtml += "</div>";
-        $cusHtml += '<div class="tab-pane fade" id="header-setting">';
-        $cusHtml += '<h5 class="mb-20">Header Skin Color</h5>';
-        $cusHtml +=
-            '<button class="cus-radio light active" data-target="header-top" data-color="light"><i class="icon"></i>light</button>';
-        $cusHtml +=
-            '<button class="cus-radio dark" data-target="header-top" data-color="dark"><i class="icon"></i>dark</button>';
-        $cusHtml +=
-            '<button class="cus-radio primary" data-target="header-top" data-color="primary"><i class="icon"></i>primary</button>';
-        $cusHtml +=
-            '<button class="cus-radio secondary" data-target="header-top" data-color="secondary"><i class="icon"></i>secondary</button>';
-        $cusHtml +=
-            '<button class="cus-radio indigo" data-target="header-top" data-color="indigo"><i class="icon"></i>indigo</button>';
-        $cusHtml +=
-            '<button class="cus-radio purple" data-target="header-top" data-color="purple"><i class="icon"></i>purple</button>';
-        $cusHtml +=
-            '<button class="cus-radio pink" data-target="header-top" data-color="pink"><i class="icon"></i>pink</button>';
-        $cusHtml +=
-            '<button class="cus-radio red" data-target="header-top" data-color="red"><i class="icon"></i>red</button>';
-        $cusHtml +=
-            '<button class="cus-radio brown" data-target="header-top" data-color="brown"><i class="icon"></i>brown</button>';
-        $cusHtml +=
-            '<button class="cus-radio orange" data-target="header-top" data-color="orange"><i class="icon"></i>orange</button>';
-        $cusHtml +=
-            '<button class="cus-radio green" data-target="header-top" data-color="green"><i class="icon"></i>green</button>';
-        $cusHtml += "</div>";
-        $cusHtml += '<div class="tab-pane fade" id="primary-setting">';
-        $cusHtml += '<h5 class="mb-20">Primary Color</h5>';
-        $cusHtml +=
-            '<button class="cus-radio color primary active" data-color="primary"><i class="icon"></i>primary</button>';
-        $cusHtml +=
-            '<button class="cus-radio color indigo" data-color="indigo"><i class="icon"></i>indigo</button>';
-        $cusHtml +=
-            '<button class="cus-radio color purple" data-color="purple"><i class="icon"></i>purple</button>';
-        $cusHtml +=
-            '<button class="cus-radio color pink" data-color="pink"><i class="icon"></i>pink</button>';
-        $cusHtml +=
-            '<button class="cus-radio color red" data-color="red"><i class="icon"></i>red</button>';
-        $cusHtml +=
-            '<button class="cus-radio color brown" data-color="brown"><i class="icon"></i>brown</button>';
-        $cusHtml +=
-            '<button class="cus-radio color orange" data-color="orange"><i class="icon"></i>orange</button>';
-        $cusHtml +=
-            '<button class="cus-radio color green" data-color="green"><i class="icon"></i>green</button>';
-        $cusHtml += "</div>";
-        $cusHtml += "</div>";
-        $cusHtml += "</div>";
-        $cusHtml += "</div>";
-
-        $body.prepend($cusHtml);
-
-        $window.on("load", function() {
-            var customizerWrapper = $(".customizer-wrapper"),
-                customizerToggle = $(".customizer-toggle"),
-                toggleDARK = customizerWrapper.find(".toggle-dark"),
-                toggleRTL = customizerWrapper.find(".toggle-rtl"),
-                customRadioNotColor = $(".cus-radio:not(.color)"),
-                customRadioColor = $(".cus-radio.color");
-
-            customizerToggle.on("click", function(e) {
-                e.preventDefault();
-                customizerWrapper.toggleClass("open");
-            });
-
-            /*Dark & RTL*/
-            toggleDARK.on("click", function() {
-                var $this = $(this);
-                if ($this.find('input[type="checkbox"]').prop("checked")) {
-                    $body.addClass("skin-dark");
-                    $body.removeClass(function(index, className) {
-                        return (
-                            className.match(/\bheader-top-\S+/g) || []
-                        ).join(" ");
-                    });
-                    $body.removeClass(function(index, className) {
-                        return (
-                            className.match(/\bside-header-\S+/g) || []
-                        ).join(" ");
-                    });
-                } else if (
-                    !$this.find('input[type="checkbox"]').prop("checked")
-                ) {
-                    $body.removeClass("skin-dark");
-                }
-            });
-            toggleRTL.on("click", function() {
-                var $this = $(this);
-                if ($this.find('input[type="checkbox"]').prop("checked")) {
-                    $body.attr("dir", "rtl");
-                } else if (
-                    !$this.find('input[type="checkbox"]').prop("checked")
-                ) {
-                    $body.removeAttr("dir", "rtl");
-                }
-            });
-
-            /*Side Header & Top Header*/
-            customRadioNotColor.on("click", function(e) {
-                e.preventDefault();
-                var $this = $(this),
-                    $target = $this.data("target"),
-                    $color = $this.data("color");
-                if ($target == "side-header") {
-                    $body.removeClass(function(index, className) {
-                        return (
-                            className.match(/\bside-header-\S+/g) || []
-                        ).join(" ");
-                    });
-                } else if ($target == "header-top") {
-                    $body.removeClass(function(index, className) {
-                        return (
-                            className.match(/\bheader-top-\S+/g) || []
-                        ).join(" ");
-                    });
-                }
-                $this
-                    .closest(".tab-pane")
-                    .find(".cus-radio")
-                    .removeClass("active");
-                $this.addClass("active");
-                $body.addClass($target + "-" + $color);
-            });
-            /*Color*/
-            customRadioColor.on("click", function(e) {
-                e.preventDefault();
-                var $this = $(this),
-                    $color = $this.data("color");
-                customRadioColor.removeClass("active");
-                $this.addClass("active");
-                $("#cus-style").attr(
-                    "href",
-                    "assets/css/style-" + $color + ".css"
-                );
-            });
-        });
-    }
-    customizer();
 
     /*--
     Custom Scrollbar (Perfect Scrollbar)
